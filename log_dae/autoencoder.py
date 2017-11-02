684f5fff847f9accc6cf42e00037179a598549ec
shandyone <1197891777@qq.com>
2017-11-02 01:37:45 -0700

    something

--- log_dae/autoencoder.py
+++ log_dae/autoencoder.py
@@ -12,12 +12,12 @@ import read_csv
 
 # Parameters
 learning_rate = 0.006
-training_epochs = 10000
+training_epochs = 20000
 batch_size = 10
 display_step = 1
 examples_to_show = 10
 time_step = 30
-corruption_level=0.25
+corruption_level=0.5
 
 def get_data():#if time_step is too long, the batch_index will be none
     #data = read_csv.data
@@ -77,41 +77,50 @@ def decoder(x):
 encoder_op = encoder(X)
 decoder_op = decoder(encoder_op)
 
-cost = tf.reduce_mean(tf.pow(X - decoder_op, 2))
-optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-
-init = tf.initialize_all_variables()
-sess = tf.InteractiveSession()
-sess.run(init)
-
-data,batch_index=get_data()
+data, batch_index = get_data()
 data_max = read_csv.data_max
 data_min = read_csv.data_min
-for epoch in range(training_epochs):
-    for i in range(len(batch_index)-1):
-        batch_xs = np.array(data[batch_index[i]:batch_index[i+1]]).reshape([-1,time_step])
-        mask_np = np.random.binomial(1, 1 - corruption_level, batch_xs.shape)
-        _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs,mask:mask_np})
+saver=tf.train.Saver(tf.global_variables())
 
-    if epoch % display_step == 0:
-        print "Epoch:", '%04d' % (epoch+1),"cost=", "{:.9f}".format(c)
+def train():
+    cost = tf.reduce_mean(tf.pow(X - decoder_op, 2))
+    optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(training_epochs):
+            for i in range(len(batch_index)-1):
+                batch_xs = np.array(data[batch_index[i]:batch_index[i+1]]).reshape([-1,time_step])
+                mask_np = np.random.binomial(1, 1 - corruption_level, batch_xs.shape)
+                _, c = sess.run([optimizer, cost], feed_dict={X: batch_xs,mask:mask_np})
 
-print "Optimization Finished!"
+            if epoch % display_step == 0:
+                print "Epoch:", '%04d' % (epoch+1),"cost=", "{:.9f}".format(c)
+        saver.save(sess, '/home/zshang/Pycharm/log_dae/model/dae.model')
+        print "Optimization Finished!"
 
+def test():
+    with tf.Session() as sess:
+        module_file = tf.train.latest_checkpoint('/home/zshang/Pycharm/log_dae/model')
+        saver.restore(sess, module_file)
+        data_test=np.array(data[2]).reshape([-1,time_step])
+        #mask_np = np.random.binomial(1, 1 - corruption_level, data_test.shape)
+        mask_np = np.ones(data_test.shape)
+        encode_decode = sess.run(decoder_op, feed_dict={X: data_test,mask:mask_np})
 
-data_test=np.array(data[10]).reshape([-1,time_step])
-mask_np = np.random.binomial(1, 1 - corruption_level, data_test.shape)
-encode_decode = sess.run(decoder_op, feed_dict={X: data_test,mask:mask_np})
+    data_test_re = (data_test.reshape([-1]))*(data_max-data_min)+data_min
+    ende = (encode_decode.reshape([-1]))*(data_max-data_min)+data_min
+    print data_test_re
+    print ende
 
-data_test_re = (data_test.reshape([-1]))*(data_max-data_min)+data_min
-ende = (encode_decode.reshape([-1]))*(data_max-data_min)+data_min
-print data_test_re
-print ende
+    plt.figure()
+    plt.plot(data_test_re,c='b')
+    plt.plot(ende,c='y')
+    plt.savefig('diff.png')
+    plt.show()
 
-plt.figure()
-plt.plot(data_test_re,c='b')
-plt.plot(ende,c='y')
-plt.show()
+if __name__ == '__main__':
+    #train()
+    test()
 # # 图像展示对比
 # f, a = plt.subplots(2, 10, figsize=(10, 2))
 # for i in range(examples_to_show):
